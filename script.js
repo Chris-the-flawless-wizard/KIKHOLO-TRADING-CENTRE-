76544cc1b87aa3dd986fa4d70cdc7c981c80f7a8
CHRIS THE FLAWLESS WIZARD <chrismukhwana456@gmail.com>
2026-06-19 13:40:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+function visit(){
+    alert("Welcome to TradeHub Online Trading Centre!");
+}
 
+function searchBusiness(){
+
+let input =
+document.getElementById("searchInput")
+.value.toLowerCase();
+
+let cards =
+document.querySelectorAll(".shop-card");
+
+cards.forEach(card => {
+
+let text =
+card.innerText.toLowerCase();
+
+if(text.includes(input)){
+card.style.display = "block";
+}else{
+card.style.display = "none";
+}
+
+});
+
+}
